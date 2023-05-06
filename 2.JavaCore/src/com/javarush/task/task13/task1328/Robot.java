7b911ddb0b625b70869981f624a0bb598c4496ad
Andrei Larionov <0658203@gmail.com>
2023-05-06 17:28:22 -0300

    solves another JR problem

--- 2.JavaCore/src/com/javarush/task/task13/task1328/Robot.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/Robot.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task13.task1328;
 
-public class Robot implements Attackable, Defensable {
-    private static int hitCount;
+public class Robot extends AbstractRobot {
+
     private String name;
 
     public Robot(String name) {
@@ -12,33 +12,4 @@ public class Robot implements Attackable, Defensable {
         return name;
     }
 
-    public BodyPart attack() {
-        BodyPart attackedBodyPart = null;
-        hitCount = hitCount + 1;
-
-        if (hitCount == 1) {
-            attackedBodyPart = BodyPart.ARM;
-        } else if (hitCount == 2) {
-            attackedBodyPart = BodyPart.HEAD;
-        } else if (hitCount == 3) {
-            hitCount = 0;
-            attackedBodyPart = BodyPart.LEG;
-        }
-        return attackedBodyPart;
-    }
-
-    public BodyPart defense() {
-        BodyPart defendedBodyPart = null;
-        hitCount = hitCount + 2;
-
-        if (hitCount == 1) {
-            defendedBodyPart = BodyPart.HEAD;
-        } else if (hitCount == 2) {
-            defendedBodyPart = BodyPart.LEG;
-        } else if (hitCount == 3) {
-            hitCount = 0;
-            defendedBodyPart = BodyPart.ARM;
-        }
-        return defendedBodyPart;
-    }
 }
